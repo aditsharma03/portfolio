eacbc4554db55aa321a89c05fd0c09fca199ff8a
Adit <aditsharma1203@gmail.com>
2024-07-17 15:50:25 +0530

    added links to entries

--- src/components/Tech/projects.ts
+++ src/components/Tech/projects.ts
@@ -3,31 +3,31 @@ const projects = [
             title: "Cyber Threat - Phishing Email Detection",
             description: "This Machine Learning project targets the various spam and malicious  emails that people receive and tries to automate the task of phishing email detection.",
             tags: ["ML", "python"],
-            link: ""
+            link: "https://github.com/aditsharma03/PhishingEmailDetection"
         },
         {
             title: "Handwritten Digit Recognition",
             description: "A Machine Learning model trained on the MNIST dataset which uses a convolutional neural network to predict value of handwritten digits and achieves an accuracy of greater than 90 percent.",
             tags: ["ML", "python"],
-            link: ""
+            link: "https://github.com/aditsharma03/HandwrittenDigitRecognition"
         },
         {
             title: "Smart Garbage Management System",
             description: "his project integrates the power of Internet of things and Machine Learning. An ultrasonic sensor is used to detect percentage filled in a dustbin, whereas a CNN model is used to categorize the image data. It tries to minimise mixed waste streams locally and thereby solving the problem globally.",
             tags: ["ML", "python"],
-            link: ""
+            link: "https://github.com/aditsharma03/waste-management-application"
         },
         {
             title: "I-Note App",
-            description: "A notes managing application developed using Node js and Mongo DB, which encorporates the CRUD operations of a database.",
+            description: "A notes managing application developed using Node js and Mongo DB, which encorporates the CRUD operations of a database.(hosted on onrender: https://i-note-app.onrender.com)",
             tags: ["NodeJS", "EJS"],
-            link: ""
+            link: "https://github.com/aditsharma03/i_note_app"
         },
         {
             title: "Chip-8 Emulator",
             description: "Developed a Chip-8 Emulator in Rust programming language, which emulates all the registers and opcodes present in the original chip-8 and can run various ROMs efficiently.",
             tags: ["Rust"],
-            link: ""
+            link: "https://github.com/aditsharma03/Chip8-Emulator"
         },
     ]
 
